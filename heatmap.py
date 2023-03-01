6a1bb13cfdc475a70a962eba56b5e84d0b0dfed3
atharva360 <68213646+atharva360@users.noreply.github.com>
2023-03-01 20:15:37 +0530

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -1,34 +1,44 @@
-from datetime import date, datetime
+import yfinance as yf
 import pandas as pd
-import calendar
+import seaborn as sns
 import matplotlib.pyplot as plt
-import seaborn as sns;sns.set_theme()
-import jugaad_data.nse as nse
 import streamlit as st
+import datetime
 
+
+ticker = "^NSEI"
+start_date = st.date_input('Enter Start date',datetime.date(2009,1, 1))
+end_date = st.date_input('Enter End date',datetime.date(2023,1, 1))
 
 st.title('NIFTY 50 HEATMAP')
-nifty = nse.index_df(symbol="NIFTY 50", from_date=date(2012,1,1), to_date=datetime.now().date())
-nifty['M'] = nifty['HistoricalDate'].dt.month
-nifty['Y'] = nifty['HistoricalDate'].dt.year
-# print(nifty)
+# Download the historical data for Reliance Industries Limited's stock
+data = yf.download(ticker, start=start_date, end=end_date)
 
-nifty.sort_values('HistoricalDate', inplace=True)
-nifty.reset_index(drop=True, inplace=True)
+# Resample the data on a monthly basis
+data_monthly = data.resample('M').last()
 
-nifty.set_index('HistoricalDate', inplace=True)
+# Calculate the monthly returns
+monthly_returns = data_monthly['Adj Close'].pct_change()
 
-nifty_monthly = nifty.resample("M").last()
+# Convert monthly returns to a pandas DataFrame
+monthly_returns_df = pd.DataFrame(monthly_returns)
 
-nifty_monthly['Returns'] = (nifty_monthly['CLOSE'] - nifty_monthly['CLOSE'].shift(1))*100/nifty_monthly['CLOSE'].shift(1)
-# print(nifty_monthly)
+# Pivot the DataFrame to create a matrix of monthly returns by year and month
+monthly_returns_matrix = monthly_returns_df.pivot_table(values='Adj Close', index=monthly_returns_df.index.year, columns=monthly_returns_df.index.month)
 
-heatmap_ret = pd.pivot_table(nifty_monthly, index='Y', columns='M', values=['Returns'])
+# Set the column names to the month names
+monthly_returns_matrix.columns = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-heatmap_ret.columns = [calendar.month_name[i] for i in range(1,13) ]
 
-ab = plt.figure(figsize=(16, 10))
-ax = sns.heatmap(heatmap_ret, cmap='RdYlGn', annot=True)
-ax.tick_params(top=True, labeltop=True)
 
+# Set the font scale
+sns.set(font_scale=1.2)
+
+# Plot the heatmap using seaborn
+ab = plt.figure(figsize=(16,10))
+sns.heatmap(monthly_returns_matrix, annot=True, cmap='RdYlGn', center=0, fmt='.2%', cbar=False)
+plt.title('Nifty Monthly Returns by Year and Month', fontsize=20)
+plt.xlabel('Month', fontsize=14)
+plt.ylabel('Year', fontsize=12)
+plt.yticks(rotation=45)
 st.pyplot(ab)
